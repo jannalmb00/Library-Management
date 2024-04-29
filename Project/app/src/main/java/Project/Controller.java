235ca70b87976de03c9ada03e54e72131272320f
jannalmb00 <2322746@edu.vaniercollege.qc.ca>
2024-04-29 17:32:00 -0400

    add Singleton and Student class

--- Project/app/src/main/java/Project/Controller.java
+++ Project/app/src/main/java/Project/Controller.java
@@ -9,5 +9,36 @@ package Project;
  * @author jannalomibao
  */
 public class Controller {
+    Library library = new Library();
+    private MainForm mainFormView;
+    private static Controller controllerObject;
+    
+    private Controller() {
+        
+    }
+    private Controller(Library library, MainForm mainFormView) {
+        this.library = library;
+        this.mainFormView = mainFormView;     
+    }
+    public static Controller getInstance() {
+        // create object if it's not already created
+        if(controllerObject == null) {
+           controllerObject = new Controller();
+        }
+         // returns the singleton object
+         return controllerObject;
+    }
+    public static Controller getInstance(Library library, MainForm mainFormView) {
+        // create object if it's not already created
+        if(controllerObject == null) {
+           controllerObject = new Controller(library, mainFormView);
+        }
+
+         // returns the singleton object
+         return controllerObject;
+    }
+    public void showForm() {
+        mainFormView.show();
+    }
     
 }
